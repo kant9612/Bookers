f3b146bba2a2f5d9a7bb20a18dc82477585d90bc
kant9612 <kanstephentakayama@gmail.com>
2021-01-10 01:40:40 +0000

    [add] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,20 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    blog = Book.new(blog_params)
+    blog.save
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
