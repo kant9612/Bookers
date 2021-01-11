2cdb56817067cf507d231d4863764ceb8553b883
kant9612 <kanstephentakayama@gmail.com>
2021-01-11 10:52:31 +0000

    [add]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,10 +19,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "successfully created"
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -52,6 +53,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
